9ff3cf339db9fae7d2af7922cda6c3698a6d1f99
christyuda <chrisyuda@ulbi.ac.id>
2024-07-29 14:44:06 +0700

    feat : buat sidebar model

---
+++ assets/js/sidebar-model.js
@@ -0,0 +1,35 @@
+// assets/js/sidebar-model.js
+
+export const sidebarItems = [
+  {
+    title: "Home",
+    link: "index.html",
+    icon: "fas fa-home",
+  },
+  {
+    title: "About Us",
+    link: "about.html",
+    icon: "fas fa-info-circle",
+  },
+  {
+    title: "Services",
+    icon: "fas fa-concierge-bell",
+    children: [
+      {
+        title: "Web Development",
+        link: "services-web-development.html",
+        icon: "fas fa-code",
+      },
+      {
+        title: "SEO",
+        link: "services-seo.html",
+        icon: "fas fa-chart-line",
+      },
+    ],
+  },
+  {
+    title: "Contact",
+    link: "contact.html",
+    icon: "fas fa-envelope",
+  },
+];
