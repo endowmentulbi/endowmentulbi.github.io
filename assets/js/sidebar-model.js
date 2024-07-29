426372479a8ca3be5733ad626dfdc67e1054a234
christyuda <chrisyuda@ulbi.ac.id>
2024-07-29 14:55:45 +0700

    feat : hapusin untuk tidak dipakai sidebar

--- assets/js/sidebar-model.js
+++ assets/js/sidebar-model.js
@@ -11,22 +11,22 @@ export const sidebarItems = [
     link: "about.html",
     icon: "fas fa-info-circle",
   },
-  {
-    title: "Services",
-    icon: "fas fa-concierge-bell",
-    children: [
-      {
-        title: "Web Development",
-        link: "services-web-development.html",
-        icon: "fas fa-code",
-      },
-      {
-        title: "SEO",
-        link: "services-seo.html",
-        icon: "fas fa-chart-line",
-      },
-    ],
-  },
+  //   {
+  //     title: "Services",
+  //     icon: "fas fa-concierge-bell",
+  //     children: [
+  //       {
+  //         title: "Web Development",
+  //         link: "services-web-development.html",
+  //         icon: "fas fa-code",
+  //       },
+  //       {
+  //         title: "SEO",
+  //         link: "services-seo.html",
+  //         icon: "fas fa-chart-line",
+  //       },
+  //     ],
+  //   },
   {
     title: "Contact",
     link: "contact.html",
