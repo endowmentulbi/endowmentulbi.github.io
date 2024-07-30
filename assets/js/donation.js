bbfc2a14b208ae20157bd910b0fb527e1889e4b4
christyuda <chrisyuda@ulbi.ac.id>
2024-07-30 15:40:01 +0700

    feat : perbaiki tombol donasi dan cara donasi

---
+++ assets/js/donation.js
@@ -0,0 +1,11 @@
+document
+  .getElementById("check-donation-button")
+  .addEventListener("click", function () {
+    window.location.href = "link-ke-cara-donasi.html";
+  });
+
+document
+  .getElementById("donation-button")
+  .addEventListener("click", function () {
+    window.location.href = "link-ke-halaman-donasi.html";
+  });
